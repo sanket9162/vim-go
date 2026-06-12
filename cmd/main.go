461bf6ed7a0e36ff42569ee9772c15f30186d9ee
sanket9162 <sanketgondhali55@gmail.com>
2026-06-12 12:40:04 +0530

    initialize tcell screen in main entry point

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"log"
+
+	"github.com/gdamore/tcell/v3"
+)
+
+func main() {
+
+	// initialize the screen
+	s, err := tcell.NewScreen()
+	if err != nil {
+		log.Fatalf("%+V", err)
+	}
+	if err = s.Init(); err != nil {
+		log.Fatalf("%+V", err)
+	}
+	defer s.Fini()
+
+}
