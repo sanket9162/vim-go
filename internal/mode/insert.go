393e19244ce608e9d710e9f834989745f2fa0873
sanket9162 <sanketgondhali55@gmail.com>
2026-06-21 09:50:50 +0530

    implement jk shortcut to exit insert mode

--- internal/mode/insert.go
+++ internal/mode/insert.go
@@ -2,7 +2,9 @@ package mode
 
 import "github.com/gdamore/tcell/v3"
 
-type InsertMode struct{}
+type InsertMode struct {
+	lastChar rune // Tracks the last character inserted to detecd sequences
+}
 
 // Name returns the name of the insert mode.
 func (m *InsertMode) Name() string { return "INSERT" }
@@ -11,15 +13,28 @@ func (m *InsertMode) Name() string { return "INSERT" }
 func (m *InsertMode) HandleKey(e EditorInterface, ev *tcell.EventKey) {
 	switch ev.Key() {
 	case tcell.KeyEscape:
+		m.lastChar = 0
 		e.SetMode("NORMAL")
 	case tcell.KeyEnter:
+		m.lastChar = 0
 		e.InsertNewline()
 	case tcell.KeyBackspace, tcell.KeyBackspace2:
+		m.lastChar = 0
 		e.DeleteChar()
 	case tcell.KeyRune:
 		runes := []rune(ev.Str())
 		for _, r := range runes {
+			// If 'j' is pressed right after 'k', delete 'k' and switch to NORMAL mode
+			if r == 'k' && m.lastChar == 'j' {
+				e.DeleteChar()
+				e.SetMode("NORMAL")
+				return
+			}
 			e.InsertChar(r)
+			m.lastChar = r
 		}
+	default:
+		// Reset tracking fo non-rune keys (like arrows, tab, etc.)
+		m.lastChar = 0
 	}
 }
