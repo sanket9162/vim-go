c0bb7d9af049ac73ae70e78785c5f557783c221b
sanket9162 <sanketgondhali55@gmail.com>
2026-06-26 18:17:22 +0530

    define Theme and EditorColors structures for UI theming configuration

---
+++ internal/ui/theme.go
@@ -0,0 +1,22 @@
+package ui
+
+type Theme struct {
+	Name   string            `json:"name"`
+	Editor EditorColors      `json:"editor"`
+	Syntax map[string]string `json:"syntax"`
+}
+
+type EditorColors struct {
+	Background              string `json:"background"`
+	Foreground              string `json:"foreground"`
+	GutterBackground        string `json:"gutter_background"`
+	GutterForeground        string `json:"gutter_foreground"`
+	SelectionBackground     string `json:"selection_background"`
+	SelectionForeground     string `json:"selection_foreground"`
+	SearchMatchBackground   string `json:"search_match_background"`
+	SearchMatchForeground   string `json:"search_match_foreground"`
+	SearchCurrentBackground string `json:"search_current_background"`
+	SearchCurrentForeground string `json:"search_current_foreground"`
+	StatusBarBackground     string `json:"status_bar_background"`
+	StatusBarForeground     string `json:"status_bar_foreground"`
+}
